52c4cdeb3b14d6b09d4667f3846f15f65c9f6c14
yit <yit@localhost.localdomain>
2019-04-15 17:12:27 +0530

    updated file api

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from util import CUR_DIR
+import csv
+import os
 from classifier import classify
 review = {}
 review['food']=[]
@@ -12,10 +14,23 @@ review['others']=[]
 
 
 def analyze_csv():
+    review = {}
+    review['food']=[]
+    review['health']=[]
+    review['journey']=[]
+    review['security']=[]
+    review['clean']=[]
+    review['digital']=[]
+    review['others']=[]
     data = pd.read_csv('./media/TestData.csv')
-
+    cats = []
     for i in range (len(data['complaint'])):
-        review[classify(data['complaint'][i])].append(data['complaint'][i])
-    print(review)
+        cat = classify(data['complaint'][i])
+        cats.append(cat)
+        review[cat].append(data['complaint'][i])
+
+    # Writing Data with Category in a file
+    cats_df = pd.DataFrame(cats,columns=["category"])
+    os.remove('./media/TestData.csv')
     return review
 # analyze_csv()
